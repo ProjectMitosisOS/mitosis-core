2a3309618450f18748ebc0c03c0dd7d675a8affc
wangtx <wang.tianxia@foxmail.com>
2021-12-21 10:02:56 +0800

    minor

--- core-daemon/src/manager/manager.rs
+++ core-daemon/src/manager/manager.rs
@@ -2,6 +2,6 @@ pub struct MitosisManager {
     // TODO
 }
 
-pub static mut MY_VM_OP: crate::bindings::vm_operations_struct = unsafe {
+pub(crate) static mut MY_VM_OP: crate::bindings::vm_operations_struct = unsafe {
     core::mem::transmute([0u8; core::mem::size_of::<crate::bindings::vm_operations_struct>()])
 };
