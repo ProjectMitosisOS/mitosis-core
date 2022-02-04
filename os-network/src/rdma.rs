8a0ea5e8f3665fac46c76448b3a4606088f94459
wxd <wxdwfc@sjtu.edu.cn>
2022-02-04 23:27:41 +0800

    add scheme code

--- os-network/src/rdma.rs
+++ os-network/src/rdma.rs
@@ -1,5 +1,3 @@
-// TODO
-
 use alloc::string::String; 
 
 pub struct ConnMeta {
@@ -8,4 +6,36 @@ pub struct ConnMeta {
     pub qd_hint: u64
 }
 
+#[derive(Debug)]
+pub enum ConnErr {
+    /// Error in finding the path whiling connecting
+    /// with raw gid
+    PathNotFound = 0,
+
+    /// Timeout error
+    Timeout,
+
+    /// Error when the qp is not ready a.k.a. RTS state
+    QPNotReady,
+
+    /// General error in the rdma connection operation
+    ConnErr,
+}
+
+pub type ConnResult<T> = Result<T, ConnErr>; 
+
+#[derive(Debug)]
+pub enum Err {
+    // TODO: Need to be refined, should be more detailed
+
+    /// Timeout error
+    Timeout = 0,
+
+    /// Other general error
+    Other, 
+}
+
+pub type IOResult<T> = Result<T, Err>;
+
 pub mod rc;
+
---
+++ os-network/src/rdma/rc.rs
@@ -0,0 +1,40 @@
+use alloc::sync::Arc;
+
+use KRdmaKit::device::RContext;
+use KRdmaKit::qp::RC;
+
+pub struct RCFactory<'a> { 
+    rcontext: &'a RContext<'a>,
+}
+
+impl crate::ConnFactory for RCFactory<'_> { 
+    type ConnMeta = super::ConnMeta;
+    type ConnType = RCConn; 
+    type ConnResult<T> =  super::ConnResult<T>; 
+
+    fn create(&mut self, meta : Self::ConnMeta) 
+        -> Self::ConnResult<Self::ConnType>
+           where Self::ConnType : crate::Conn { 
+        unimplemented!(); 
+    }
+}
+
+pub struct RCConn {
+    rc: Arc<RC>
+}
+
+impl crate::Conn for RCConn { 
+    type IOResult<T> = super::IOResult<T>; 
+    type ReqPayload =  u64;
+    type CompPayload = u64; 
+
+    fn post(&mut self, req : &Self::ReqPayload) -> Self::IOResult<()> { 
+        unimplemented!();
+    }
+
+    fn poll(&mut self) -> Self::IOResult<Self::CompPayload> {
+        unimplemented!();
+    }
+
+
+}
