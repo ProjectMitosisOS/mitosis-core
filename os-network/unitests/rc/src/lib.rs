db1ddbcfbb431e3cda55ab76c1f96917c521d23d
wxd <wxdwfc@sjtu.edu.cn>
2022-02-06 15:47:39 +0800

    refine code

--- os-network/unitests/rc/src/lib.rs
+++ os-network/unitests/rc/src/lib.rs
@@ -40,15 +40,59 @@ fn test_rc_factory() {
     }; 
 
     let mut rc = client_factory.create(conn_meta).unwrap();
-    log::debug!("{:?}", rc.get_status().unwrap());  
+    let status = rc.get_status();
+    if status.is_some() { 
+        log::info!("test connect w/o meta passes! {:?}", status.unwrap()); 
+    } else { 
+        log::error!("unable to get the connection meta"); 
+    }
 }
 
-fn test_rc_factory_with_meta() {}
+fn test_rc_factory_with_meta() {
+    use KRdmaKit::KDriver;
+    let driver = unsafe { KDriver::create().unwrap() };
+    let client_nic = driver
+        .devices()
+        .into_iter()
+        .next()
+        .expect("no rdma device available"); 
+    
+    let mut client_factory = rdma::rc::RCFactoryWPath::new(client_nic).unwrap(); 
+
+    let server_ctx = driver
+        .devices()
+        .into_iter()
+        .next()
+        .expect("no rdma device available")
+        .open()
+        .unwrap();
+
+    let server_service_id: u64 = 0;
+    let _ctrl = RCtrl::create(server_service_id, &server_ctx);
+
+
+    // the main test body
+    let conn_meta = rdma::ConnMeta { 
+        gid : server_ctx.get_gid_as_string(), 
+        service_id : server_service_id, 
+        qd_hint : 0
+    }; 
+    let conn_meta = client_factory.convert_meta(conn_meta).unwrap(); 
+
+    let mut rc = client_factory.create(conn_meta).unwrap();
+    let status = rc.get_status();
+    if status.is_some() { 
+        log::info!("test connect w/o meta passes! {:?}", status.unwrap()); 
+    } else { 
+        log::error!("unable to get the connection meta"); 
+    }    
+}
 
 impl linux_kernel_module::KernelModule for SampleTestModule {
     fn init() -> linux_kernel_module::KernelResult<Self> {
         log::info!("test started"); 
         test_rc_factory(); 
+        test_rc_factory_with_meta();
         Ok(Self {})
     }
 }
--- os-network/unitests/rc/tests/test.rs
+++ os-network/unitests/rc/tests/test.rs
@@ -1,4 +1,4 @@
-use kernel_module_testlib::{with_kernel_module,assert_dmesg_ends,assert_dmesg_contains, dmesg_contains};
+use kernel_module_testlib::{with_kernel_module, dmesg_contains};
 
 #[test]
 fn test_basic() {
