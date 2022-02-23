b37018d4cae9b20e337c4e50ecd27b59e457d1af
wangtx <wang.tianxia@foxmail.com>
2022-02-23 14:22:16 +0800

    minor

--- os-network/bench/sample/src/lib.rs
+++ os-network/bench/sample/src/lib.rs
@@ -14,6 +14,6 @@ fn test() {}
 #[krdma_test(test)]
 fn ctx_init() {
     log::info!("context init");
-    log::info!("samle_int: {}", config::get_sample_int());
+    log::info!("sample_int: {}", config::get_sample_int());
     log::info!("sample_long: {}", config::get_sample_long());
 }
