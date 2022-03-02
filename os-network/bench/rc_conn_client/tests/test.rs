197a062839906563b4c3f7e9ce121f9846d713b1
wangtx <wang.tianxia@foxmail.com>
2022-03-02 16:19:44 +0800

    remove unused test code

--- os-network/bench/rc_conn_client/tests/test.rs
+++
@@ -1,9 +0,0 @@
-use kernel_module_testlib::{with_kernel_module, dmesg_contains};
-
-#[test]
-fn test_params() {
-    // a dummy test func
-    with_kernel_module(|| {
-        assert_eq!(dmesg_contains(&String::from("error")),false);
-    });
-}
