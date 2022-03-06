5d9c19988873a163ebcd181bc6793f9bf421c2b7
wangtx <wang.tianxia@foxmail.com>
2022-03-06 15:45:47 +0800

    refactor server side module

--- os-network/bench/rc_conn_server/src/lib.rs
+++ os-network/bench/rc_conn_server/src/lib.rs
@@ -5,7 +5,7 @@ extern crate alloc;
 use alloc::boxed::Box;
 use alloc::vec::Vec;
 
-use krdma_test::{krdma_test, krdma_drop};
+use core::pin::Pin;
 
 use linux_kernel_module::c_types::*;
 
@@ -21,44 +21,61 @@ use rust_kernel_linux_util::linux_kernel_module;
 use rust_kernel_linux_util as log;
 
 declare_global!(KDRIVER, alloc::boxed::Box<KRdmaKit::KDriver>);
-declare_global!(RCTRLS, alloc::boxed::Box<alloc::vec::Vec<core::pin::Pin<alloc::boxed::Box<KRdmaKit::ctrl::RCtrl<'static>>>>>);
 declare_global!(RCONTEXTS, alloc::boxed::Box<alloc::vec::Vec<KRdmaKit::device::RContext<'static>>>);
 
-fn module_main() {
-    let driver = unsafe { KDRIVER::get_ref() };
+struct Module<'a> {
+    rctrls: Vec<Pin<Box<RCtrl<'a>>>>,
+}
 
-    for i in 0..nic_count::read() {
-        log::info!("create context {} on nic {}", i, i);
-        let ctx = driver
-            .devices()[i as usize]
-            .open()
-            .unwrap();
-        unsafe { RCONTEXTS::get_mut().push(ctx) };
-    }
+impl linux_kernel_module::KernelModule for Module<'_> {
+    
+    fn init() -> linux_kernel_module::KernelResult<Self> {
+        
+        unsafe { KDRIVER::init(KDriver::create().unwrap()) };
+        unsafe { RCONTEXTS::init(Box::new(Vec::new())) };
 
-    for i in 0..nic_count::read() {
-        let service_id = service_id_base::read();
-        log::info!("create RCtrl {} on context {}", service_id, i);
-        let rctrl = RCtrl::create(service_id as u64, unsafe { &RCONTEXTS::get_ref()[i as usize] }).unwrap();
-        unsafe { RCTRLS::get_mut().push(rctrl) };
+        let driver = unsafe { KDRIVER::get_ref() };
+        let mut rctrls = Vec::new();
+        
+        for i in 0..nic_count::read() {
+            let ctx = driver
+                .devices()[i as usize]
+                .open()
+                .unwrap();
+            unsafe { 
+                RCONTEXTS::get_mut().push(ctx);
+            };
+        }
+        
+        for i in 0..nic_count::read() {
+            let service_id = service_id_base::read();
+            log::info!("create RCtrl {} on device {}", service_id, i);
+            let rctrl = RCtrl::create(service_id as u64, unsafe { &RCONTEXTS::get_ref()[i as usize] });
+            rctrls.push(rctrl.unwrap());
+        }
+        
+        Ok(Self {
+            rctrls: rctrls,
+        })
     }
 }
 
-#[krdma_test(module_main)]
-fn ctx_init() {
-    unsafe {
-        KDRIVER::init(KDriver::create().unwrap());
-        RCONTEXTS::init(Box::new(Vec::new()));
-        RCTRLS::init(Box::new(Vec::new()));
+impl Drop for Module<'_> {
+
+    fn drop(&mut self) {
+        self.rctrls.clear();
+        unsafe {
+            RCONTEXTS::drop();
+            KDRIVER::drop();
+        }
     }
 }
 
-#[krdma_drop]
-fn module_drop() {
-    unsafe {
-        RCTRLS::drop();
-        RCONTEXTS::drop();
-        KDRIVER::drop();
-    };
-    log::info!("context drop");
-}
+linux_kernel_module::kernel_module!(
+    Module,
+    author: b"xmm",
+    description: b"Test test framework in the kernel",
+    license: b"GPL"
+);
+
+unsafe impl Sync for Module<'_> {}
