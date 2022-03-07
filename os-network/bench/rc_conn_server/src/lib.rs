3fd472aca3c90f732b965f69c2f0ae8f5303ade5
wxd <wxdwfc@sjtu.edu.cn>
2022-03-07 11:08:23 +0800

    minor

--- os-network/bench/rc_conn_server/src/lib.rs
+++ os-network/bench/rc_conn_server/src/lib.rs
@@ -9,59 +9,56 @@ use core::pin::Pin;
 
 use linux_kernel_module::c_types::*;
 
-use KRdmaKit::KDriver;
 use KRdmaKit::ctrl::RCtrl;
+use KRdmaKit::KDriver;
 
-use mitosis_macros::{declare_module_param, declare_global};
+use mitosis_macros::{declare_global, declare_module_param};
 
 declare_module_param!(nic_count, c_uint);
 declare_module_param!(service_id_base, c_uint);
 
-use rust_kernel_linux_util::linux_kernel_module;
 use rust_kernel_linux_util as log;
+use rust_kernel_linux_util::linux_kernel_module;
 
 declare_global!(KDRIVER, alloc::boxed::Box<KRdmaKit::KDriver>);
-declare_global!(RCONTEXTS, alloc::boxed::Box<alloc::vec::Vec<KRdmaKit::device::RContext<'static>>>);
+declare_global!(
+    RCONTEXTS,
+    alloc::boxed::Box<alloc::vec::Vec<KRdmaKit::device::RContext<'static>>>
+);
 
 struct Module<'a> {
     rctrls: Vec<Pin<Box<RCtrl<'a>>>>,
 }
 
 impl linux_kernel_module::KernelModule for Module<'_> {
-    
     fn init() -> linux_kernel_module::KernelResult<Self> {
-        
         unsafe { KDRIVER::init(KDriver::create().unwrap()) };
         unsafe { RCONTEXTS::init(Box::new(Vec::new())) };
 
         let driver = unsafe { KDRIVER::get_ref() };
         let mut rctrls = Vec::new();
-        
+
         for i in 0..nic_count::read() {
-            let ctx = driver
-                .devices()[i as usize]
-                .open()
-                .unwrap();
-            unsafe { 
+            let ctx = driver.devices()[i as usize].open().unwrap();
+            unsafe {
                 RCONTEXTS::get_mut().push(ctx);
             };
         }
-        
+
         for i in 0..nic_count::read() {
             let service_id = service_id_base::read();
             log::info!("create RCtrl {} on device {}", service_id, i);
-            let rctrl = RCtrl::create(service_id as u64, unsafe { &RCONTEXTS::get_ref()[i as usize] });
+            let rctrl = RCtrl::create(service_id as u64, unsafe {
+                &RCONTEXTS::get_ref()[i as usize]
+            });
             rctrls.push(rctrl.unwrap());
         }
-        
-        Ok(Self {
-            rctrls: rctrls,
-        })
+
+        Ok(Self { rctrls: rctrls })
     }
 }
 
 impl Drop for Module<'_> {
-
     fn drop(&mut self) {
         self.rctrls.clear();
         unsafe {
@@ -73,7 +70,7 @@ impl Drop for Module<'_> {
 
 linux_kernel_module::kernel_module!(
     Module,
-    author: b"xmm",
+    author: b"wtx",
     description: b"Test test framework in the kernel",
     license: b"GPL"
 );
