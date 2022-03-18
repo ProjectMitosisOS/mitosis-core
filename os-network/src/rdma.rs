9b5b824ee7e1ac80b9c539eaebbaa0e5e7f78bf0
wangtx <wang.tianxia@foxmail.com>
2022-03-18 22:22:52 +0800

    fix wc errno

--- os-network/src/rdma.rs
+++ os-network/src/rdma.rs
@@ -4,6 +4,7 @@ use alloc::string::String;
 
 use KRdmaKit::device::RContext;
 use KRdmaKit::rust_kernel_rdma_base::sa_path_rec;
+// use KRdmaKit::rust_kernel_rdma_base::bindings::ib_wc_status;
 
 pub struct ConnMeta {
     pub gid: String,
@@ -46,7 +47,7 @@ pub enum ConnErr {
 pub enum Err {
     // TODO: Need to be refined, should be more detailed
     /// Timeout error
-    Timeout = 0,
+    Timeout,
 
     /// Retry, used to indicate retrying the erroneous function call, typically `ib_poll_cq`
     Retry,
@@ -55,6 +56,45 @@ pub enum Err {
     /// Other general error
     ///     
     Other,
+    /// ib_wc error
+    /// 
+    WCErr(WCStatus),
+}
+
+#[repr(C)]
+#[allow(non_camel_case_types)]
+#[derive(Debug, PartialEq)]
+pub enum WCStatus {
+    IB_WC_SUCCESS,
+	IB_WC_LOC_LEN_ERR,
+	IB_WC_LOC_QP_OP_ERR,
+	IB_WC_LOC_EEC_OP_ERR,
+	IB_WC_LOC_PROT_ERR,
+	IB_WC_WR_FLUSH_ERR,
+	IB_WC_MW_BIND_ERR,
+	IB_WC_BAD_RESP_ERR,
+	IB_WC_LOC_ACCESS_ERR,
+	IB_WC_REM_INV_REQ_ERR,
+	IB_WC_REM_ACCESS_ERR,
+	IB_WC_REM_OP_ERR,
+	IB_WC_RETRY_EXC_ERR,
+	IB_WC_RNR_RETRY_EXC_ERR,
+	IB_WC_LOC_RDD_VIOL_ERR,
+	IB_WC_REM_INV_RD_REQ_ERR,
+	IB_WC_REM_ABORT_ERR,
+	IB_WC_INV_EECN_ERR,
+	IB_WC_INV_EEC_STATE_ERR,
+	IB_WC_FATAL_ERR,
+	IB_WC_RESP_TIMEOUT_ERR,
+	IB_WC_GENERAL_ERR,
+}
+
+impl From<u32> for WCStatus {
+    fn from(errno: u32) -> Self {
+        unsafe {
+            core::mem::transmute(errno)
+        }
+    }
 }
 
 #[derive(Debug)]
