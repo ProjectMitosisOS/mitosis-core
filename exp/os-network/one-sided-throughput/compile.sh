58ee73de37ef41df565b8aee62bce48f62b56c1e
wangtx <wang.tianxia@foxmail.com>
2022-03-22 21:28:23 +0800

    update scripts

--- exp/os-network/one-sided-throughput/compile.sh
+++ exp/os-network/one-sided-throughput/compile.sh
@@ -2,6 +2,8 @@
 pushd ../../../os-network/bench \
 && python3 build.py one_sided_read_server \
 && python3 build.py dc_read_client \
+&& python3 build.py rc_read_client \
 && popd \
 && cp ../../../os-network/bench/one_sided_read_server_tests.ko . \
-&& cp ../../../os-network/bench/dc_read_client_tests.ko .
+&& cp ../../../os-network/bench/dc_read_client_tests.ko . \
+&& cp ../../../os-network/bench/rc_read_client_tests.ko .
