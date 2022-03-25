120304198d399504945e319252506ba8494d5faf
wxd <wxdwfc@sjtu.edu.cn>
2022-03-25 12:49:45 +0800

    first step of RDMA context

--- mitosis/src/lib.rs
+++ mitosis/src/lib.rs
@@ -1,13 +1,14 @@
 #![no_std]
+#![feature()]
+
+extern crate alloc;
 
 mod bindings;
 
-use os_network::KRdmaKit;
+pub(crate) use os_network::KRdmaKit;
 
-#[allow(unused_imports)]
-pub(crate) use KRdmaKit::rust_kernel_rdma_base::linux_kernel_module;
-#[allow(unused_imports)]
-pub(crate) use KRdmaKit::rust_kernel_rdma_base::rust_kernel_linux_util as log;
+pub use rust_kernel_linux_util as log;
+pub use KRdmaKit::rust_kernel_rdma_base::linux_kernel_module;
 
 pub const VERSION: usize = 0;
 
@@ -16,10 +17,16 @@ pub mod syscalls;
 // The RDMA context used by MITOSIS
 pub mod rdma_context;
 
+use alloc::vec::Vec;
+
 #[derive(Debug)]
 pub struct Config {
     pub(crate) num_nics_used: usize,
     pub(crate) fallback_threads_num: usize,
+    // my machine ID
+    pub(crate) machine_id: usize,
+    // gid is RDMA address
+    pub(crate) peers_gid: Vec<alloc::string::String>,
 }
 
 impl Default for Config {
@@ -27,18 +34,48 @@ impl Default for Config {
         Self {
             num_nics_used: 1,
             fallback_threads_num: 2,
+            machine_id: 0,
+            peers_gid: Vec::new(),
         }
     }
 }
 
-impl Config { 
-    pub fn set_num_nics_used(&mut self, num : usize) -> &mut self { 
+impl Config {
+    pub fn set_num_nics_used(&mut self, num: usize) -> &mut Self {
         self.num_nics_used = num;
         self
     }
 
-    pub fn set_fallback_num(&mut self, num : usize) -> &mut self { 
+    pub fn set_fallback_num(&mut self, num: usize) -> &mut Self {
         self.fallback_threads_num = num;
         self
     }
+
+    pub fn set_machine_id(&mut self, id: usize) -> &mut Self {
+        self.machine_id = id;
+        self
+    }
+
+    pub fn add_gid(&mut self, gid: alloc::string::String) -> &mut Self {
+        self.peers_gid.push(gid);
+        self
+    }
 }
+
+use mitosis_macros::declare_global;
+
+// kernel-space global variables
+
+/***** RDMA-related global data structures */
+
+use KRdmaKit::device::RContext;
+use KRdmaKit::ctrl::RCtrl;
+use KRdmaKit::rust_kernel_rdma_base::*;
+
+// low-level contexts are directly exposed as global variables 
+declare_global!(rdma_driver, alloc::boxed::Box<crate::KRdmaKit::KDriver>);
+declare_global!(rdma_contexts, alloc::vec::Vec<crate::RContext<'static>>);
+
+// high-level contexts are abstracted in rdma_context.rs
+
+/***** RDMA-related global data structures ends*/
--- mitosis/src/rdma_context.rs
+++ mitosis/src/rdma_context.rs
@@ -1,3 +1,36 @@
-pub struct Context { 
-    
+use alloc::vec::Vec;
+
+use os_network::KRdmaKit;
+use KRdmaKit::ctrl::RCtrl;
+use KRdmaKit::rust_kernel_rdma_base::*;
+
+use os_network::rpc::*;
+
+/// Note: need to call `end_rdma` before destroying the kernel module
+/// Return
+/// * Some(()), the start succeeds
+/// * None, fatal error happens, the result is printed
+pub fn start_rdma(config: crate::Config) -> core::option::Option<()> {
+    unsafe {
+        let kdriver = KRdmaKit::KDriver::create()?;
+        crate::rdma_driver::init(kdriver);
+    };
+
+    unsafe {
+        let mut contexts = Vec::new();
+        for i in 0..config.num_nics_used {
+            contexts.push(crate::rdma_driver::get_mut().devices().get(i)?.open()?);
+        }
+        crate::rdma_contexts::init(contexts);
+    };
+
+    Some(())
+}
+
+pub fn end_rdma() {
+    // Note: the **order** of drop is very important here
+    unsafe {
+        crate::rdma_contexts::drop();
+        crate::rdma_driver::drop();
+    };
 }
