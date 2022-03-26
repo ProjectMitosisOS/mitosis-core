cf91a3a7861de9bbc7e73765e1d9ebb517c01a60
wangtx <wang.tianxia@foxmail.com>
2022-03-26 15:51:56 +0800

    image descriptor

--- mitosis/src/descriptor.rs
+++ mitosis/src/descriptor.rs
@@ -1,9 +1,37 @@
+use alloc::vec::Vec;
+
+use os_network::bytes::BytesMut;
+use os_network::rdma::RawGID;
+
+use hashbrown::HashMap;
+
+type AddrType = u64;
+
+pub struct RemoteMachine {
+    gid: RawGID,
+    service_id: u64,
+    rkey: u32,
+}
+
+pub struct RemotePage {
+    addr: AddrType,
+    dct_key: u32,
+}
+
+pub struct VMAMeta {
+    range: (AddrType, AddrType), // [start,end] of a vma range
+    flags: crate::bindings::vm_flags_t,
+    prot: crate::bindings::pgprot_t,
+}
+
 #[allow(dead_code)]
 pub struct Descriptor {
-    regs: (),
-    page_table: (),
-    dct_key: (),
-    // TODO: more need to be added
+    regs: crate::bindings::PTRegs,
+    page_table: HashMap::<u64, RemotePage>,
+    vma: Vec<VMAMeta>,
+    machine: RemoteMachine,
+    fs: u64,
+    gs: u64,
 }
 
 impl os_network::serialize::Serialize for Descriptor {
