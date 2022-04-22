34114c6d2a327a05deb1a08de9d576478b70240d
wangtx <wang.tianxia@foxmail.com>
2022-04-22 16:50:27 +0800

    use cached namespace; assign containeer name in the commandline

--- mitosis-user-libs/mitosis-lean-container/lib/tests/test_start_app.c
+++ mitosis-user-libs/mitosis-lean-container/lib/tests/test_start_app.c
@@ -10,31 +10,31 @@
 #define MAX_ENV_VAR_COUNT 256
 
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        printf("Usage: %s [/path/to/rootfs] [command (absolute path)] [command opts]\n", argv[0]);
+    if (argc < 4) {
+        printf("Usage: %s [container name] [/path/to/rootfs] [command (absolute path)] [command opts]\n", argv[0]);
         return -1;
     }
     
-    char* rootfs_path = argv[1];
-    char* command = argv[2];
-    char* name = "test";
+    char* name = argv[1];
+    char* rootfs_path = argv[2];
+    char* command = argv[3];
     char* execve_argv[MAX_COMMAND_LENGTH];
     char* execve_envp[MAX_ENV_VAR_COUNT];
     int argv_index = 0;
 
     // setup argv array
-    for (int i = 2; i < argc && argv_index < MAX_COMMAND_LENGTH; i++, argv_index++)
+    for (int i = 3; i < argc && argv_index < MAX_COMMAND_LENGTH; i++, argv_index++)
         execve_argv[argv_index] = argv[i];
     execve_argv[argv_index] = NULL;
 
     // setup envp array
-    // TODO: support more 
+    // TODO: support more environment variables
     execve_envp[0] = "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
     execve_envp[1] = NULL;
 
     struct ContainerSpec spec;
     int ret;
-    pid_t pid;
+    pid_t pid, cached_namespace;
     
     // unlimited resources
     spec.cpu_start = -1;
@@ -49,11 +49,13 @@ int main(int argc, char* argv[]) {
     ret = add_lean_container_template(name, &spec);
     assert(ret == 0);
 
+    cached_namespace = setup_cached_namespace();
+
     // setup the lean container of `name`
     // and the rootfs of the lean container is specified by second parameter
-    pid = setup_lean_container(name, rootfs_path, -1);
+    pid = setup_lean_container(name, rootfs_path, cached_namespace);
     if (pid < 0) {
-        printf("set lean container failed!");
+        printf("set lean container failed!\n");
         goto clean;
     }
 
@@ -81,7 +83,7 @@ int main(int argc, char* argv[]) {
         printf("unable to unpause container");
     }
 
-    pid_t child = waitpid(-1, NULL, 0);
+    pid_t child = waitpid(pid, NULL, 0);
     if (child != pid) {
         printf("child pid: %d, expected: %d\n", child, pid);
     }
@@ -91,6 +93,9 @@ clean:
     assert(ret == 0);
 
     ret = deinit_cgroup();
+    assert(ret == 0);
+
+    ret = remove_cached_namespace(cached_namespace);
     assert(ret == 0);
 
     printf("pass lean container unit test!\n");
