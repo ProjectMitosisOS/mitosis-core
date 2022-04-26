34fbea8b087651a20deccac7f6abfaa479ec9640
Fangming Lu <lfm1012@sjtu.edu.cn>
2022-04-26 19:56:29 +0800

    remove exp-child

--- exp/fork-functions/micro/bench.py
+++
@@ -1,8 +0,0 @@
-import time
-
-
-def report(name, start, end):
-    passed_us = (end - start) * 1000000
-    print("[%s] time: %.2f us" % (str(name), passed_us))
-
-
--- exp/scripts/run_lean_container.sh
+++ exp/scripts/run_lean_container.sh
@@ -3,6 +3,8 @@ exe_path=../bench_lean_container
 bench_sec=5
 name=bench_lean_container
 rootfs_path=/home/lfm/projects/mos/mitosis-user-libs/mitosis-lean-container/.base/hello/rootfs
+
+#
 command="/app/lean_child"
 
 sudo $exe_path $bench_sec $name $rootfs_path $command 1 73
