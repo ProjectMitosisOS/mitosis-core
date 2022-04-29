16f16808357c6bca9711d514305f440a80503046
Fangming Lu <lfm1012@sjtu.edu.cn>
2022-04-29 12:32:11 +0800

    update

---
+++ exp/common/mitosis_wrapper.py
@@ -0,0 +1,75 @@
+import argparse
+import os
+import time
+from functools import wraps
+import syscall_lib
+import bench
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-handler_id", type=int, default=73, help="rfork handler id")
+parser.add_argument("-working_set", type=int, default=16777216,
+                    help="working set size")
+parser.add_argument("-exclude_execution", type=int, default=1,
+                    help="Whether exclude the resume stage")
+parser.add_argument("-profile", type=int, default=1, help="whether print out the profile data")
+parser.add_argument("-pin", type=int, default=0, help="whether pin the descriptor in kernel")
+parser.add_argument("-touch_ratio", type=int, default=100, help="child touch ratio")
+parser.add_argument("-app_name", type=str, default="micro", help="application name")
+args = parser.parse_args()
+
+handler_id = args.handler_id
+working_set = args.working_set
+profile = args.profile
+pin = args.pin
+app_name = args.app_name
+touch_ratio = args.touch_ratio
+ret_imm = args.exclude_execution != 0
+ret = ret_imm == 1
+
+
+def mitosis_bench(handler):
+    """
+    mitosis benchmark
+        Usage
+            @mitosis_bench
+            def bench():
+                my_handler()
+    """
+
+    def prepare(key):
+        fd = syscall_lib.open()
+        start = time.time()
+        if pin == 1:
+            syscall_lib.call_prepare_ping(fd, key)
+        else:
+            syscall_lib.call_prepare(fd, key)
+        end = time.time()
+        if profile == 1:
+            print("done...")
+            bench.report("%s-prepare" % app_name, start, end)
+
+    @wraps(handler)
+    def wrapper(*args, **kwargs):
+        result = handler(*args, **kwargs)
+        prepare(handler_id)
+        if not ret_imm:
+            result = handler(*args, **kwargs)
+        os._exit(0)
+
+    return wrapper
+
+
+def tick_execution_time(handler):
+    """
+
+    """
+    @wraps(handler)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = handler(*args, **kwargs)
+        end = time.time()
+        if profile == 1:
+            bench.report("%s-execution" % app_name, start, end)
+        return result
+
+    return wrapper
--- exp/fork-functions/chamelon/function.py
+++ exp/fork-functions/chamelon/function.py
@@ -1,40 +1,17 @@
 import os
 import sys
-import time
-import json
-import random
-import requests
 
 sys.path.append("../../common")  # include outer path
-import syscall_lib
-import bench
 
-os.environ['OPENBLAS_NUM_THREADS'] = '1'
-os.environ['MKL_NUM_THREADS'] = '1'
+import bench
+from mitosis_wrapper import *
 from chameleon import PageTemplate
 import six
 
-## Migration related
-import argparse
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+os.environ['MKL_NUM_THREADS'] = '1'
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-handler_id", type=int, default=73, help="rfork handler id")
-parser.add_argument("-exclude_execution", type=int, default=1,
-                    help="Whether exclude the resume stage")
-parser.add_argument("-profile", type=int, default=1, help="whether print out the profile data")
-parser.add_argument("-pin", type=int, default=0, help="whether pin the descriptor in kernel")
-parser.add_argument("-app_name", type=str, default="micro", help="application name")
 
-args = parser.parse_args()
-
-handler_id = args.handler_id
-ret_imm = args.exclude_execution != 0
-profile = args.profile
-pin = args.pin
-app_name = args.app_name
-ret = ret_imm == 1
-
-## Migration related end
 BIGTABLE_ZPT = """\
 <table xmlns="http://www.w3.org/1999/xhtml"
 xmlns:tal="http://xml.zope.org/namespaces/tal">
@@ -48,9 +25,8 @@ tal:content="python: d" />
 </table>""" % six.text_type.__name__
 
 
+@tick_execution_time
 def handler():
-    start = time.time()
-
     num_of_rows = 4
     num_of_cols = 4
 
@@ -62,28 +38,11 @@ def handler():
     table = [data for x in range(num_of_rows)]
     options = {'table': table}
     data = tmpl.render(options=options)
-    end = time.time()
-    if profile == 1:
-        bench.report("%s-execution" % app_name, start, end)
 
-
-def prepare(key):
-    fd = syscall_lib.open()
-    start = time.time()
-    if pin == 1:
-        print("pin to the kernel")
-        syscall_lib.call_prepare_ping(fd, key)
-    else:
-        syscall_lib.call_prepare(fd, key)
-    end = time.time()
-    if profile == 1:
-        bench.report("%s-prepare" % app_name, start, end)
+@mitosis_bench
+def bench():
+    handler()
 
 
 if __name__ == '__main__':
-    handler()
-    prepare(handler_id)
-    if not ret_imm:
-        handler()
-    # print("done")
-    os._exit(0)
+    bench()
