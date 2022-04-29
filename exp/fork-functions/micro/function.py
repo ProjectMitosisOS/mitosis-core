3ab005cfc2d5c513532276bdfb9edbbda30236cb
Fangming Lu <lfm1012@sjtu.edu.cn>
2022-04-29 16:20:41 +0800

    update

--- exp/fork-functions/micro/function.py
+++ exp/fork-functions/micro/function.py
@@ -1,30 +1,71 @@
 import os
 import sys
 import time
-import mmap
 
 sys.path.append("../../common")  # include outer path
+
 import syscall_lib
 import bench
-from mitosis_wrapper import *
+# import json
+# import random
+# import requests
 
+import argparse
+import mmap
 
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
 os.environ['MKL_NUM_THREADS'] = '1'
-working_set = working_set
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-handler_id", type=int, default=73, help="rfork handler id")
+parser.add_argument("-working_set", type=int, default=16777216,
+                    help="working set size")
+parser.add_argument("-exclude_execution", type=int, default=1,
+                    help="Whether exclude the resume stage")
+parser.add_argument("-profile", type=int, default=1, help="whether print out the profile data")
+parser.add_argument("-pin", type=int, default=0, help="whether pin the descriptor in kernel")
+parser.add_argument("-touch_ratio", type=int, default=100, help="child touch ratio")
+parser.add_argument("-app_name", type=str, default="micro", help="application name")
+
+args = parser.parse_args()
+
+handler_id = args.handler_id
+working_set = args.working_set
+ret_imm = args.exclude_execution != 0
+profile = args.profile
+pin = args.pin
+app_name = args.app_name
+touch_ratio = args.touch_ratio
+ret = ret_imm == 1
+
 mm = mmap.mmap(-1, length=working_set)
 
 
-@tick_execution_time
 def handler(working_sz):
+    start = time.time()
     mm.seek(0)
-    mm.read(working_sz)
+    r = mm.read(working_sz)
+    end = time.time()
+    if profile == 1:
+        bench.report("%s-execution" % app_name, start, end)
 
 
-@mitosis_bench
-def bench():
-    handler(working_sz=working_set)
+def prepare(key):
+    fd = syscall_lib.open()
+    start = time.time()
+    if pin == 1:
+        syscall_lib.call_prepare_ping(fd, key)
+    else:
+        syscall_lib.call_prepare(fd, key)
+    end = time.time()
+    if profile == 1:
+        bench.report("%s-prepare" % app_name, start, end)
 
 
 if __name__ == '__main__':
-    bench()
+    handler(working_set)
+    prepare(handler_id)
+    if not ret_imm:
+        handler(working_set * touch_ratio // 100)
+    # print("done")
+    os._exit(0)
