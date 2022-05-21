ff81cfd8be28992390a25437cecee1e4695af714
wxd <wxdwfc@gmail.com>
2022-05-21 16:39:23 +0800

    minor: page lookup

--- mitosis/src/remote_mapping/page_table.rs
+++ mitosis/src/remote_mapping/page_table.rs
@@ -3,6 +3,7 @@ pub use x86_64::{
     PhysAddr, VirtAddr,
 };
 
+
 use alloc::boxed::Box;
 
 use super::page_structures::*;
@@ -10,6 +11,7 @@ use super::page_structures::*;
 pub type RemotePage = Page<Size4KiB>; 
 
 /// Abstracts a (remote) forked page table
+/// We do this by emulating the four-level page table of x86
 ///
 /// Limitation:
 /// - we only support 4KiB page table now
@@ -19,24 +21,33 @@ pub type RemotePage = Page<Size4KiB>;
 pub struct RemotePageTable {
     // note: we use the box as the inner data type/
     // otherwise, this data structure can easily overflow the kernel stack
-    inner_page_table: Box<PageTable>,
+    l4_page_table: Box<PageTable>,
 }
 
 impl RemotePageTable {
     /// create an empty page table
     pub fn new() -> Self {
         Self {
-            inner_page_table: Box::new(PageTable::new()),
+            l4_page_table: Box::new(PageTable::new()),
         }
     }
 
     /// check whether all entries in the pagetable is zero 
     pub fn is_empty(&self) -> bool { 
-        for entry in self.inner_page_table.iter() { 
+        for entry in self.l4_page_table.iter() { 
             if *entry != 0  { 
                 return false;
             }
         }
         return true;
+    }
+
+    pub fn lookup(&self, addr : VirtAddr) -> core::option::Option<PhysAddr> { 
+        let entry = RemotePage::containing_address(addr);
+        let l3_pt = self.l4_page_table[usize::from(entry.p4_index())] as *mut PageTable;
+        if l3_pt.is_null() { 
+            return None;
+        }
+        Some(PhysAddr::new(0))
     }
 }
