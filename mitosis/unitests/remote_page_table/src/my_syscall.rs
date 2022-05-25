c39baa37ab169f065a3f7478011ae7c029dba9e7
wxd <wxdwfc@sjtu.edu.cn>
2022-05-25 14:07:17 +0800

    remote page table iterator done

--- mitosis/unitests/remote_page_table/src/my_syscall.rs
+++ mitosis/unitests/remote_page_table/src/my_syscall.rs
@@ -13,6 +13,7 @@ impl MySyscallHandler {
     fn handle_page_mapping_test(&self, arg: c_ulong) -> c_long {
         crate::log::debug!("handle page mapping, with arg {}", arg);
 
+        // initialize the page table as the caller tasks's pagetable
         let task = Task::new();
         let (_, pt) = task.generate_mm();
 
@@ -35,13 +36,34 @@ impl MySyscallHandler {
             counter += 1;
         }
 
+        // test the basic transalate works
         for (k, v) in pt.iter() {
             let nv = new_pt.translate(VirtAddr::new(*k));
             assert!(nv.is_some());
             assert_eq!(nv.unwrap().as_u64(), *v);
-        }        
+        }
 
         crate::log::debug!("check page table entry counts: {}", counter);
+
+        // now check the iterators
+        crate::log::info!("Now check the iterators");
+
+        let iter = RemotePageTableIter::new(&mut new_pt).unwrap();
+        crate::log::debug!("sanity check iter {:?}", iter);
+
+        let mut counter2 = 0;
+        for _addr in iter {
+            // crate::log::info!("check addr {:?}", addr);
+            counter2 += 1;
+        }
+
+        crate::log::info!(
+            "sanity check iterator counter: {}, previous: {} should match",
+            counter2,
+            counter
+        );
+        assert_eq!(counter, counter2);
+
         0
     }
 }
