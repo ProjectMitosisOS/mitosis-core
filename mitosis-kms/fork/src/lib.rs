b779bf341cec2e298c251381a273659f65a4c85d
Fangming Lu <lfm1012@sjtu.edu.cn>
2022-06-07 21:09:00 +0800

    update

--- mitosis-kms/fork/src/lib.rs
+++ mitosis-kms/fork/src/lib.rs
@@ -28,38 +28,6 @@ impl linux_kernel_module::KernelModule for Module {
         
         assert!(start_instance(config.clone()).is_some());
 
-        // now make some simple checks for self-called RPC
-        // now we can call!
-        let pool_idx = 0;
-        let caller = unsafe {
-            mitosis::rpc_caller_pool::CallerPool::get_global_caller(pool_idx)
-                .expect("the caller should be properly inited")
-        };
-
-        let self_session_id = mitosis::startup::calculate_session_id(
-            config.get_machine_id(),
-            pool_idx,
-            config.get_max_core_cnt(),
-        );
-
-        caller
-            .sync_call(
-                self_session_id,                                      // remote session ID
-                mitosis::rpc_handlers::RPCId::Echo as _, // RPC ID
-                0xffffffff as u64,                       // send an arg of u64
-            )
-            .unwrap();
-
-        let res = block_on(caller);
-        match res {
-            Ok(v) => {
-                let (msg, reply) = v; // msg, reply
-                log::debug!("sanity check rpc two call result: {:?}", reply);
-                caller.register_recv_buf(msg).expect("register msg buffer cannot fail");
-            }
-            Err(e) => log::error!("client call error: {:?}", e),
-        };
-
         Ok(Self {
             service: SysCallsService::<MitosisSysCallHandler>::new()?,
         })
