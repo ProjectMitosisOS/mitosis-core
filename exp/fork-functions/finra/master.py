0e65aa437e2aae8f95b671faeadb1b6d430de988
Fangming Lu <lfm1012@sjtu.edu.cn>
2022-06-20 16:54:14 +0800

    update

--- exp/fork-functions/finra/master.py
+++ exp/fork-functions/finra/master.py
@@ -4,7 +4,6 @@ import time
 
 import zerorpc
 import util
-from agileutil.rpc.server import rpc, TcpRpcServer
 
 start = time.time()
 counter = 0
@@ -16,48 +15,43 @@ def report(name, start, end):
     sys.stdout.flush()
 
 
-@rpc
-def private_data(event):
-    startTime = 1000 * time.time()
+class RPCServer(object):
 
-    portfolio = event['body']['portfolio']
+    def private_data(self, event):
+        startTime = 1000 * time.time()
 
-    data = util.portfolios[portfolio]
+        portfolio = event['body']['portfolio']
 
-    valid = True
+        data = util.portfolios[portfolio]
 
-    for trade in data:
-        side = trade['Side']
-        # Tag ID: 552, Tag Name: Side, Valid values: 1,2,8
-        if not (side == 1 or side == 2 or side == 8):
-            valid = False
-            break
+        valid = True
 
-    response = {'statusCode': 200, 'body': {'valid': valid, 'portfolio': portfolio}}
-    endTime = 1000 * time.time()
-    return util.timestamp(response, event, startTime, endTime, 0)
+        for trade in data:
+            side = trade['Side']
+            # Tag ID: 552, Tag Name: Side, Valid values: 1,2,8
+            if not (side == 1 or side == 2 or side == 8):
+                valid = False
+                break
+        response = {'statusCode': 200, 'body': {'valid': valid, 'portfolio': portfolio}}
+        endTime = 1000 * time.time()
+        return util.timestamp(response, event, startTime, endTime, 0)
 
+    def tick_rule_start(self):
+        global start, counter
+        counter = 0
+        start = time.time()
 
-@rpc
-def tick_rule_start():
-    global start, counter
-    counter = 0
-    start = time.time()
+    def report_finish_event(self):
+        global start, counter
+        counter += 1
+        end = time.time()
+        report("tick %d" % counter, start, end)
 
-
-@rpc
-def report_finish_event():
-    global start, counter
-    counter += 1
-    end = time.time()
-    report("tick %d" % counter, start, end)
-
-
-@rpc
-def exit():
-    global server
-    os._exit(0)
+    def exit(self):
+        global server
+        os._exit(0)
 
 
-server = TcpRpcServer('0.0.0.0', 8090)
-server.serve()
+s = zerorpc.Server(RPCServer())
+s.bind("tcp://0.0.0.0:8090")
+s.run()
