c027df7486e8f640b2095a4a4ca1d8b05ed0fb51
Fangming Lu <lfm1012@sjtu.edu.cn>
2022-06-23 19:52:25 +0800

    reconstruct framework

--- exp/fork-functions/finra/trigger.py
+++ exp/fork-functions/finra/trigger.py
@@ -1,39 +1,34 @@
 import argparse
 import socket
 
-import zerorpc
-
 # python trigger.py -child_hosts=val04 -process=2
+import time
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-port", type=int, default=8080, help="rpc server port")
 parser.add_argument("-child_hosts", type=str, default="", help="rpc server host")
 parser.add_argument("-parent_host", type=str, default="localhost", help="parent host")
 parser.add_argument("-process", type=int, default=1, help="rpc parallel num")
 args = parser.parse_args()
-port = args.port
 process = args.process
 parent_host = args.parent_host
 child_hosts = str(args.child_hosts).split(' ')
-
-master_cli = zerorpc.Client()
-master_cli.connect("tcp://%s:%d" % ("127.0.0.1", 8090))
+master_port = 7000
+parent_port = 8000
+trigger_port = 9000
 
 s_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s_udp.sendto(str(3 + len(child_hosts) * process).encode(), ('', master_port))
 
-s_tcp.connect(("localhost", 6000))
-# start
-master_cli.tick_rule_start(process * len(child_hosts))
+s_tcp.connect((parent_host, parent_port))
+s_tcp.sendall(parent_host.encode())
 
-s_tcp.sendall(b"data")
+start = time.time()  # tick start
 s_tcp.recv(1024).decode()
 
-if args.child_hosts == '':
-    master_cli.report_finish_event()
-else:
-    for host in child_hosts:
-        # Trigger without waiting
-        for i in range(process):
-            s_udp.sendto(b"data", (host, port + i))
+for host in child_hosts:
+    # Trigger without waiting
+    for i in range(process):
+        s_udp.sendto(b"data", (host, parent_port + i))
 s_tcp.close()
 s_udp.close()
