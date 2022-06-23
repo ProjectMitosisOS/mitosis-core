381fe92f30e382260cb22de9c5b936fff41fb779
Fangming Lu <lfm1012@sjtu.edu.cn>
2022-06-23 09:40:34 +0800

    update

--- exp/fork-functions/finra/trigger.py
+++ exp/fork-functions/finra/trigger.py
@@ -6,7 +6,7 @@ import zerorpc
 # python trigger.py -child_hosts=val04 -process=2
 parser = argparse.ArgumentParser()
 parser.add_argument("-port", type=int, default=8080, help="rpc server port")
-parser.add_argument("-child_hosts", type=str, default="localhost", help="rpc server host")
+parser.add_argument("-child_hosts", type=str, default="", help="rpc server host")
 parser.add_argument("-parent_host", type=str, default="localhost", help="parent host")
 parser.add_argument("-process", type=int, default=1, help="rpc parallel num")
 args = parser.parse_args()
@@ -27,10 +27,14 @@ master_cli.tick_rule_start(process * len(child_hosts))
 
 s_tcp.sendall(b"data")
 s_tcp.recv(1024).decode()
-for host in child_hosts:
-    # Trigger without waiting
-    for i in range(process):
-        s_udp.sendto(b"data", (host, port + i))
 
+if args.child_hosts == '':
+    print("finish parent bench")
+    master_cli.report_finish_event()
+else:
+    for host in child_hosts:
+        # Trigger without waiting
+        for i in range(process):
+            s_udp.sendto(b"data", (host, port + i))
 s_tcp.close()
 s_udp.close()
