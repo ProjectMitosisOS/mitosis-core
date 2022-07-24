bd129d1b25b2e8d83a9e437c50cd12367976175d
wangtx <wang.tianxia@foxmail.com>
2022-07-24 20:50:15 +0800

    stress test scripts

---
+++ stress-test/fork/simple_parent.cc
@@ -0,0 +1,33 @@
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <gflags/gflags.h>
+
+#include "../../mitosis-user-libs/mitosis-c-client/include/syscall.h"
+
+DEFINE_int64(handler_id, 73, "rfork handler id");
+DEFINE_bool(pin, true, "whether pin in kernel");
+
+int
+main(int argc, char *argv[]) {
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    int sd = sopen();
+    int cnt = 0;
+    assert(sd != 0);
+
+    if (FLAGS_pin) {
+        fork_prepare_ping(sd, FLAGS_handler_id);
+        // return immediately
+        _exit(0);
+    } else {
+        sleep(1);
+        printf("time %d\n", cnt++);
+        fork_prepare(sd, FLAGS_handler_id);
+    }
+
+    while (1) {
+        printf("time %d\n", cnt++);
+        sleep(1);
+    }
+}
