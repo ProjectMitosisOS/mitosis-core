b9448fe6db4f05241ea738b700d6007d099223a3
Zhidong Guo <gungnir_guo@sjtu.edu.cn>
2023-01-05 20:37:41 +0800

    refactor: revise DCQP and RPC to use lock bundler instead of global locks

--- mitosis/src/lock_bundler.rs
+++ mitosis/src/lock_bundler.rs
@@ -1,7 +1,8 @@
-use alloc::boxed::Box;
-
 use crate::linux_kernel_module::mutex::LinuxMutex;
+use alloc::boxed::Box;
 use rust_kernel_linux_util::linux_kernel_module::sync::Mutex;
+
+pub type BoxedLockBundler<T> = Box<LockBundler<T>>;
 
 /// A simple wrapper over LinuxMutex to simplifiy creation
 pub struct LockBundler<T> {
@@ -9,6 +10,8 @@ pub struct LockBundler<T> {
 }
 
 impl<T> LockBundler<T> {
+    // `LinuxMutex` is wrapped in a `Box` because it is a self-referening struct that 
+    // cannot be moved.
     #[inline]
     pub fn new(inner: T) -> Box<Self> {
         let res = Box::new(Self {
