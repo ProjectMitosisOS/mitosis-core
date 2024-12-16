ea7d27a4771c59263d1c2a4c291cfd62840b00eb
hongruixie <hrxie00@outlook.com>
2024-12-16 13:30:07 +0800

    fix: minor

--- exp/criu-micro/host_dump.sh
+++ exp/criu-micro/host_dump.sh
@@ -6,6 +6,6 @@ echo "TARGET_PID=${TARGET_PID}"
 rm -rf imgs && mkdir imgs
 sleep 3
 # ~/mitosis/criu/criu/criu dump --images-dir=./imgs -t ${TARGET_PID} -vvvv -o dump.log
-/mitosis/criu/criu/criu dump --images-dir=./imgs -t ${TARGET_PID} -vvvv -o dump.log
+criu dump --images-dir=./imgs -t ${TARGET_PID} -vvvv -o dump.log
 tail -n 1 imgs/dump.log
 echo -n 1 > lock
--- exp/criu-micro/restore.sh
+++ exp/criu-micro/restore.sh
@@ -2,6 +2,6 @@
 # echo "before criu restore: $START"
 # echo ${PWD}
 # /home/xhr/mitosis/criu/criu/criu restore --images-dir=./imgs -vvvvv -o restore.log
-/mitosis/criu/criu/criu restore --images-dir=./imgs -o restore.log
+criu restore --images-dir=./imgs -o restore.log
 # END=$(date +%s.%N)
 # TOTAL_TIME=$(echo "($END-$START)*1000" | bc) # uncomment this to calculate the full end-to-end time
